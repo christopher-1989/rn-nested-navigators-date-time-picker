14c085e22934f6776d00fcc18c24bd5012cdf035
Christopher McDonald <c.robin.mcdonald@gmail.com>
2021-08-22 12:54:37 +1000

    Nested navigators functioning Stack - >Tab navigator >Stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Add the new stack navigator above this line
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+const HomeNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={HomeScreen} />
+    <Tab.Screen name="Settings" component={SettingsScreen}/>
+  </Tab.Navigator>
+)
+
+const HomeScreen = ({ navigation }) => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Home</Text>
+    <Button title="Feed" onPress={() => navigation.navigate("Feed")} />
+    <Button title="Messages" onPress={() => navigation.navigate("Messages")} />
+  </View>
+);
+
+const SettingsScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Settings</Text>
+  </View>
+);
+
+const FeedScreen = ({ navigation }) => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed</Text>
+    <Button title="Feed Pics" onPress={() => navigation.navigate("Feed Pics")} />
+
+  </View>
+);
+
+const FeedPicsScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Feed Pics</Text>
+  </View>
+);
+
+const MessagesScreen = () => (
+  <View style={styles.layout}>
+    <Text style={styles.title}>Messages</Text>
+  </View>
+);
+
+
+export const AppNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Group>
+        <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="Feed Pics" component={FeedPicsScreen} />
+      </Stack.Group>
+      <Stack.Group>
+        <Stack.Screen name="Messages" component={MessagesScreen} />
+      </Stack.Group>
+    </Stack.Navigator>  
+);
+
+const App = () => (
+  <NavigationContainer>
+    <AppNavigator />
+  </NavigationContainer>
+);
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 16,
   },
 });
